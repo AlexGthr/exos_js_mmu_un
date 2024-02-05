fb2ae71fd603273d0491d9228361d5457f4a9830
AlexGthr <agauthier5@outlook.fr>
2024-02-05 10:08:41 +0100

    Ajout de commentaires

--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
+// On récupère l'élément qui nous intéresse (ici la class .carre)
 let element = document.querySelector('.carre')
-style = getComputedStyle(element, null)
 
+// On crée une function pour définir les éléments que l'on veux récuperer
 function getInfosElement(element) {
+
+    // On crée une variable qui va contenir le style de notre élément
     let style = getComputedStyle(element, null)
 
+    // Puis on déclare des variables par rapport au style
     const className = "Class : " + element.className + "\n";
     const backgroundColor = "- Background color : " + style.backgroundColor + "\n";
     const color = "- Color : " + style.color + "\n";
@@ -12,11 +16,14 @@ function getInfosElement(element) {
     const display = " - Display : " + style.display + "\n";
     const fontFamily = " - Font Family : " + style.fontFamily + "\n";
 
+    // On déclare une variable qui contient le tout
     textAlert = className + backgroundColor + color + height + width + display + fontFamily;
 
+    // Puis on lance une alert popup pour tout afficher
     alert(textAlert);
 }
 
+// Grâce au click sur l'element, on lancera la function getInfosElement
 element.addEventListener('click', function() {
     getInfosElement(element);
 });
